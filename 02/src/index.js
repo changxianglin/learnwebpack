985b7488903951ecbcf2413279873732bcb3f083
changxianglin <281532612@qq.com>
2022-08-03 01:59:40 +0800

    css bundle

---
+++ 02/src/index.js
@@ -0,0 +1,8 @@
+import './index.css'
+
+var root = document.querySelector('.root')
+
+var element = document.createElement('h1')
+element.innerText = 'webpack !!!'
+
+root.appendChild(element)
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### learnwebpack
 
 some file about learn webpack and config start or run a project.
+
+why need to use webpack?
+1. http request
+2. source large
